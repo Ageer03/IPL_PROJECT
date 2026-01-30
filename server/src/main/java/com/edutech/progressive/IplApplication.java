2d6117295a2f73f4f6e210dd4790fdb67c2fa954
abhishek-mishraji <dummy@gmail.com>
2026-01-30 06:51:48 +0000

    D 1

--- server/src/main/java/com/edutech/progressive/IplApplication.java
+++ server/src/main/java/com/edutech/progressive/IplApplication.java
@@ -2,6 +2,8 @@ package com.edutech.progressive;
 
 public class IplApplication {
     public static void main(String[] args) {
+        //Day 1
         System.out.println("Welcome to Ipl Progressive Project!");
+        
     }
 }
